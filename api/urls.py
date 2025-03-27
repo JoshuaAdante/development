9560967baf6f0b56e74ce71702cc9a25a12dad0e
JoshuaAdante <joshua.adante@urios.edu.ph>
2025-03-27 01:53:03 +0000

    first commit

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import HelloWorld
+from .views import Students
+from .views import ContactListView
+
+urlpatterns = [
+ path('hello/', HelloWorld.as_view(), name='hello_world'),
+ path('students/', Students.as_view(), name='list_students'),
+ path('contact/', ContactListView.as_view(), name='contact_new'),
+]
